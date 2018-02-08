c9145e189fe768191002a9b1b2648a48d558795a
ICE <ice.sumy@gmail.com>
2018-02-09 00:18:25 +0100

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, z);
+        
+        var y = Input.GetAxis("Mouse X") * Time.deltaTime * 50.0f;
+        transform.Rotate(0, y, 0);
+    }
 }
